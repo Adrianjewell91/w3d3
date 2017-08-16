e9f070408a19932bb1b8989e11238317792c823a
App Academy Student <student@appacademy.io>
2017-08-16 15:42:58 -0700

    Url shortening methods built

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,3 +1,18 @@
 class ShortenedUrl < ApplicationRecord
-  validates :user_id, :long_url, presence: true 
+  validates :long_url, presence: true, uniqueness: true
+
+  def self.random_code
+    code = SecureRandom.urlsafe_base64
+    while ShortenedUrl.exists?(code)
+      code = SecureRandom.urlsafe_base64
+    end
+    code
+  end
+
+  def self.create_and_generate(user, long_url)
+    ShortenedUrl.create!(user_id: user.id,
+                        long_url: long_url,
+                        short_url: self.random_code)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
 
 
 
+
 end
